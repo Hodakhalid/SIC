ac571aede6b8322a2deb5673f587df1dfcc473ce
Huda Khalid <hodakhalid391@gmail.com>
2026-08-01 21:33:22 +0300

    Update script.js

---
+++ Assignment-1/script.js
@@ -0,0 +1,55 @@
+function start() {
+    alert("Welcome..❤️");
+
+    const start = confirm("Are you sure you want to calculate your grade?");
+
+    let  state ; 
+    if (start) {
+        let grade = Number(prompt("Enter Your grade"));
+
+        if (grade >= 90) {
+            alert("A");
+            state = "A";
+        
+        }
+
+        else if(grade >= 80){
+            alert("B");
+            state = "B";
+        }
+
+        else if(grade >= 70){
+            alert("C");
+            state = "C";
+        }
+
+        else if(grade >= 50){
+            alert("D");
+            state = "D";
+        }
+
+        else{
+            alert("F");
+            state = "F";
+        }
+
+        switch(state){
+            case "A":
+                alert("Congratulations.🎉🎉");
+                break;
+
+            case "B":
+                alert("Very Very Good.🎊🎊");
+                break;
+
+            case "C":
+                alert("Good.💖");
+                break;
+            case "D":
+                alert("Better luck next time!😊");
+                break;
+            default:
+                alert("Don't give up! Better luck next time.💪")
+        }
+    }
+}
